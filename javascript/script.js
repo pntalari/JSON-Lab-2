09b7096510ee45a334b271fe7696463c50b7cac5
Nirmala <nirmala.talari@gmail.com>
2019-03-24 12:34:10 -0400

    LabComplete

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD");
+xhttp.responseType = 'text';
+xhttp.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(xhttp.response);
+    var movies = data["data"];
+    movies = movies.filter(x => x[10] == "Golden Gate Bridge").map(x =>
+     x[8]+" in year: "+x[9]+" by production: "+x[12] + "<br>"
+    );
+    var result = movies.join("\n");
+    document.getElementById('result').innerHTML += result;
+  }
+};
+xhttp.send();
